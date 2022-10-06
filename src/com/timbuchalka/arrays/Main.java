1b92647f7ff40f844a2986edc92ef2562186f7db
Angelique Abacajan <ma_utsey@yahoo.com>
2022-10-06 15:50:47 -0400

    Implemented a for loop to loop the array infinitely

--- src/com/timbuchalka/arrays/Main.java
+++ src/com/timbuchalka/arrays/Main.java
@@ -7,9 +7,21 @@ public class Main {
     public static void main(String[] args) {
 
         int[] myIntArray = new int[10]; // { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }
-            myIntArray[0] = 45;
-            myIntArray[1] = 476;
-            myIntArray[5] = 50;
+//            myIntArray[0] = 45;
+//            myIntArray[1] = 476;
+//            myIntArray[5] = 50;
+
+        for (int i = 0; i < 10; i++) {
+
+            myIntArray[i] = i * 10;
+
+        }
+
+        for (int i = 0; i < myIntArray.length; i++) {
+
+            System.out.println("Element " + i + ", value is");
+
+        }
 
     }
 
