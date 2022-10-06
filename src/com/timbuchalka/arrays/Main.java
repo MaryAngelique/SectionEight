0718b446010326db0752d16cca2db1a9ed783dac
Angelique Abacajan <ma_utsey@yahoo.com>
2022-10-06 12:35:09 -0400

    Implemented my Int Array

--- src/com/timbuchalka/arrays/Main.java
+++ src/com/timbuchalka/arrays/Main.java
@@ -6,8 +6,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] myVariable;
-        myVariable = new int[10];
+        int[] myIntArray = new int[10]; // { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }
+            myIntArray[0] = 45;
+            myIntArray[1] = 476;
+            myIntArray[5] = 50;
 
     }
 
