f4f378c26bfafe3bf2c6efe944f645f3be31f065
Angelique Abacajan <ma_utsey@yahoo.com>
2022-10-07 13:50:58 -0400

    Implemented a for loop with an if statement to return the sorted array

--- src/com/timbuchalka/arrays/Challenge.java
+++ src/com/timbuchalka/arrays/Challenge.java
@@ -62,6 +62,18 @@ public class Challenge {
 
         while(flag) {
             flag = false;
+
+            for(int i = 0; i < sortedArray.length - 1; i++) {
+
+                if(sortedArray[i] < sortedArray[i+1]) {
+
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i+1];
+                    sortedArray[i+1] = temp;
+                    flag = true;
+
+                }
+            }
         }
 
         return sortedArray;
