6931fb3e8af0d83eedfa92b67a50c67ea0e7bedb
Angelique Abacajan <ma_utsey@yahoo.com>
2022-10-10 12:24:38 -0400

    Implemented ArrayList and addGroceryItem method

--- src/com/timbuchalka/ListAndArrays/GroceryList.java
+++ src/com/timbuchalka/ListAndArrays/GroceryList.java
@@ -2,6 +2,10 @@ package com.timbuchalka.ListAndArrays;
 
 public class GroceryList {
 
+    private ArrayList<String> groceryList = new ArrayList<String>();
 
+    public void addGroceryItem(String item) {
+        groceryList.add(item);
+    }
 
 }
