17702cb7f5bfc7c9a732adec5ca9592752ab629b
Angelique Abacajan <ma_utsey@yahoo.com>
2022-10-10 13:24:59 -0400

    Implemented Scanner and GroceryList private methods

--- src/com/timbuchalka/ListAndArrays/Main.java
+++ src/com/timbuchalka/ListAndArrays/Main.java
@@ -1,6 +1,12 @@
 package com.timbuchalka.ListAndArrays;
 
+import java.util.Scanner;
+
 public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+    private static GroceryList groceryList = new GroceryList();
+
 
     public static void main(String[] args) {
 
