8ea258f6172c69913452c0a0c9006d3eb8120361
Angelique Abacajan <ma_utsey@yahoo.com>
2022-10-09 23:20:33 -0400

    Implemented readIntegers method and a for loop

--- src/com/timbuchalka/referenceTypes/Challenge.java
+++ src/com/timbuchalka/referenceTypes/Challenge.java
@@ -15,4 +15,22 @@ public class Challenge {
 
     }
 
+    private static int[] readIntegers(int count) {
+
+        int[] array = new int[count];
+
+        for(int i = 0; i < array.length; i++) {
+
+            System.out.println("Enter a number:");
+
+            int number = scanner.nextInt();
+            scanner.nextLine();
+            array[i] = number;
+
+        }
+
+        return array;
+    }
+
+
 }
