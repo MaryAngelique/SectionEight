2829ed9694308a443219300b6ff9b4541ba03cdb
Angelique Abacajan <ma_utsey@yahoo.com>
2022-10-09 23:57:35 -0400

    Implemented reverse method, maxIndex & haldLength values, and a for loop

--- src/com/timbuchalka/ReverseArrayChallenge.java
+++ src/com/timbuchalka/ReverseArrayChallenge.java
@@ -12,4 +12,19 @@ public class ReverseArrayChallenge {
 
     }
 
+    private static void reverse(int[] array) {
+
+        int maxIndex = array.length -1;
+        int halfLength = array.length / 2;
+
+        for(int i = 0; i < halfLength; i++) {
+
+            int temp = array[i];
+
+            array[i] = array[maxIndex -i];
+            array[maxIndex - i] = temp;
+
+        }
+    }
+
 }
