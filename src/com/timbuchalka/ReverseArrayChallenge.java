04c1aec07b4e326800d0a4894eb97c3b82841da4
Angelique Abacajan <ma_utsey@yahoo.com>
2022-10-09 23:52:04 -0400

    Implemented Int array of 6 indexes

--- src/com/timbuchalka/ReverseArrayChallenge.java
+++ src/com/timbuchalka/ReverseArrayChallenge.java
@@ -6,7 +6,9 @@ public class ReverseArrayChallenge {
 
     public static void main(String[] args) {
 
+        int[] array = { 1, 5, 3, 7, 11, 9, 15};
 
+        System.out.println("Array = " + Arrays.toString(array));
 
     }
 
