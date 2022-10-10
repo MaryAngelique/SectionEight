15905e32c953bcce3d31a6323a4d68a683639fa4
Angelique Abacajan <ma_utsey@yahoo.com>
2022-10-09 22:57:02 -0400

    Implemented Scanner method

--- src/com/timbuchalka/referenceTypes/Challenge.java
+++ src/com/timbuchalka/referenceTypes/Challenge.java
@@ -1,6 +1,10 @@
 package com.timbuchalka.referenceTypes;
 
+import java.util.Scanner;
+
 public class Challenge {
+
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
