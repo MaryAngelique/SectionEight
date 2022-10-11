c631f169edc84a351ce77a5d23ef01eeaf75023d
Angelique Abacajan <ma_utsey@yahoo.com>
2022-10-11 16:07:37 -0400

    Implemented processArrayList method in Main class

--- src/com/timbuchalka/ListAndArrays/Main.java
+++ src/com/timbuchalka/ListAndArrays/Main.java
@@ -115,4 +115,19 @@ public class Main {
         }
     }
 
+    public static void processArrayList() {
+
+        ArrayList<String> newArray = new ArrayList<String>();
+        newArray.addAll(groceryList.getGroceryList());
+
+        ArrayList<String> nextArray = new ArrayList<String>(groceryList.getGroceryList());
+
+        String[] myArray = new String[groceryList.getGroceryList().size()];
+        myArray = groceryList.getGroceryList().toArray(myArray);
+
+    }
+
+
+
+
 }
