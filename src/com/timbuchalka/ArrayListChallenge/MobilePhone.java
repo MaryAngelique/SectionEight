fff075ec9989453598fc1540950f771e4b920335
Angelique Abacajan <ma_utsey@yahoo.com>
2022-10-12 09:02:46 -0400

    Implemented addNewContact method

---
+++ src/com/timbuchalka/ArrayListChallenge/MobilePhone.java
@@ -0,0 +1,28 @@
+package com.timbuchalka.ArrayListChallenge;
+
+import java.util.ArrayList;
+
+public class MobilePhone {
+
+    private String myNumber;
+    private ArrayList<Contact> myContacts;
+
+    public MobilePhone(String myNumber) {
+        this.myNumber = myNumber;
+        this.myContacts = new ArrayList<Contact>();
+    }
+
+    public boolean addNewContact(Contact contact) {
+
+        if(findContact(contact.getName()) >= 0) {
+
+            System.out.println("Contact is already on file");
+            return false;
+
+        }
+
+        myContacts.add(contact);
+        return true;
+    }
+
+}
