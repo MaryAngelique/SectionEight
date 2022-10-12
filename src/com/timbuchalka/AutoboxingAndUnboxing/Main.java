3fea15d8a7ab2c3250cf59d0f7da46fb16ef50a6
Angelique Abacajan <ma_utsey@yahoo.com>
2022-10-12 15:14:23 -0400

    Implemented ArrayList in main method

---
+++ src/com/timbuchalka/AutoboxingAndUnboxing/Main.java
@@ -0,0 +1,18 @@
+package com.timbuchalka.AutoboxingAndUnboxing;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[]  args) {
+
+        String[] strArray = new String[10];
+        int[] intArray = new int[10];
+
+        ArrayList<String> strArrayList = new ArrayList<String>();
+        strArrayList.add("Tim");
+
+
+    }
+
+}
