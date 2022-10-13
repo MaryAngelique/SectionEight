6cd03b099363e1db35aee10713eb89cff579cdb6
Angelique Abacajan <ma_utsey@yahoo.com>
2022-10-13 13:26:28 -0400

    Implemented ArrayList within Branch class

---
+++ src/com/timbuchalka/AutoboxingAndUnboxing/Challenge/Branch.java
@@ -0,0 +1,12 @@
+package com.timbuchalka.AutoboxingAndUnboxing.Challenge;
+
+import java.util.ArrayList;
+
+public class Branch {
+
+    private String name;
+    private ArrayList<Customer> customers;
+
+
+
+}
