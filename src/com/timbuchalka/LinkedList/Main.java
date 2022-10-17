fcba1a41d5fe23be9e9525cc56a81e302e49263b
Angelique Abacajan <ma_utsey@yahoo.com>
2022-10-16 22:06:28 -0400

    Implemented a for lopp to loop through the intList and return the intList of i

--- src/com/timbuchalka/LinkedList/Main.java
+++ src/com/timbuchalka/LinkedList/Main.java
@@ -28,6 +28,12 @@ public class Main {
 
         intList.add(1, 2);
 
+        for(int i = 0; i < intList.size(); i++) {
+
+            System.out.println(i +": " + intList.get(i));
+
+        }
+
     }
 
 }
