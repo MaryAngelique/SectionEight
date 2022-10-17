a81f2487c30ea37d8c6b62584023d4001f252631
Angelique Abacajan <ma_utsey@yahoo.com>
2022-10-17 11:41:50 -0400

    Implemented Song title and duration constructor

--- src/com/timbuchalka/LinkedList/Challenge/Song.java
+++ src/com/timbuchalka/LinkedList/Challenge/Song.java
@@ -5,6 +5,8 @@ public class Song {
     private String title;
     private double duration;
 
-
-
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
 }
