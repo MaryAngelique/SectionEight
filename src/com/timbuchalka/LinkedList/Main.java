bb780859cc1a2b9c8b3375c2c88038bd31ef188a
Angelique Abacajan <ma_utsey@yahoo.com>
2022-10-16 21:09:33 -0400

    Implemneted Customer and anotherCustomer in Main class

--- src/com/timbuchalka/LinkedList/Main.java
+++ src/com/timbuchalka/LinkedList/Main.java
@@ -4,7 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Customer customer = new Customer("Tim", 54.96);
+        Customer anotherCustomer;
 
+        anotherCustomer = customer;
+        anotherCustomer.setBalance(12.18);
+
+        System.out.println("Balance for customer " + customer.getName() + " is " + customer.getBalance());
 
     }
 
