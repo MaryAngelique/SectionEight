d596b06d81669a5057ea543f40615396850ee21d
Angelique Abacajan <ma_utsey@yahoo.com>
2022-10-16 23:16:53 -0400

    Implemented LinkedList in Demo class

---
+++ src/com/timbuchalka/LinkedList/Demo.java
@@ -0,0 +1,15 @@
+package com.timbuchalka.LinkedList;
+
+import java.util.LinkedList;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        LinkedList<String> placesToVisit = new LinkedList<String>();
+
+
+
+    }
+
+}
